ef33e92b3ff5bbce46e69a5fe231a25dae42d107
andrewCharlesBrown <andrew.charles.brown@protonmail.com>
2017-03-21 15:51:18 -0500

    Update RegExpressionsAndXML.ps1

--- XML_RegEx/RegExpressionsAndXML.ps1
+++ XML_RegEx/RegExpressionsAndXML.ps1
@@ -10,9 +10,10 @@ $nodeCount = $XmlDocument.dataroot.tblPhoneNumbers.Count
 #loop through file, testing number format with regular expression 
 for($i = 0; $i -lt $nodeCount; $i++) {
 
-	# point to node indexed by counter $i
+    # point to node indexed by counter $i
     $node = $XmlDocument.dataroot.tblPhoneNumbers[$i]
-
+    
+    # output formating errors to console 
     if($node.Phone_Numbers) {
         if(($node.Phone_Numbers -match  "\d{3}\.\d{3}\.\d{4}") -eq $False) {
             Write-Host "The number" $node.Phone_Numbers  "associated with" $node.First_Name $node.Last_Name "is in invalid format."
